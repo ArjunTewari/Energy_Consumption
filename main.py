001bff198f7d3290a7c280c7e9015f06a9cf8b07
Arjun Tewari <arjuntewari0505@gmail.com>
2025-02-13 19:02:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -100,5 +100,5 @@ page = """
 
 if __name__== "__main__":
     app = Gui(page)
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port, use_reloader=False, debug=False)
